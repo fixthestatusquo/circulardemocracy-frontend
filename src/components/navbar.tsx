39d05adfcd4fb902f6fe50f7433223800e91a04f
xavier <github@tttp.eu>
2026-02-07 10:10:36 +0800

    preparing new theme colors

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,8 +24,9 @@ export function Navbar() {
   return (
     <NavigationMenu className="fixed top-0 left-0 w-full max-w-none flex items-center justify-between p-4 border-b border-gray-200 z-50 bg-white">
       <div className="flex items-center">
-        <Link to="/">
+        <Link to="/" className="flex items-center space-x-2 dark:text-white text-2xl md:text-xl ml-2 rtl:ml-0 rtl:mr-2 self-center text-gray-900 whitespace-nowrap">
           <img src={logo} alt="Circular Democracy Logo" className="h-8" />
+          <span className="font-bold hidden md:block">Circular Democracy</span>
         </Link>
       </div>
       <NavigationMenuList className="flex items-center space-x-4">
